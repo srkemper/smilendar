1388c37b4e778af56699fd2f696e88e11a48ac6e
srkemper <srkemper@stanford.edu>
2014-02-10 12:02:39 -0800

    grunt setup!

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+	    jshint: {
+	      // uglify task configuration goes here.
+	      files: ['routes/*.js'],
+	      options: {
+	      	globals: {
+	      		jQuery: true,
+	      		console: true,
+	      		module: true
+	      	}
+	      }
+	    },
+	    watch: {
+	    	files: ['routes/*.js', '*.js'],
+	    	tasks: ['jshint', 'express:dev'],
+	    	options: {
+	    		spawn: false
+	    	}
+	    },
+	    handlebars: {
+
+	    },
+	    nodemon: {
+	    	dev: {
+	    		options: {
+	    			file: 'app.js',
+	    		}
+	    	}
+	    },
+	    express: {
+	    	options: {
+	    		delay: 250
+	    	},
+    		dev: {
+    			options: {
+	    			script: 'app.js'
+    			}
+    		}
+	    },
+	    shell: {
+	    	launchExpress: {
+	    		options: {
+	    			stdout: true,
+	    			execOptions: {
+	    				killSignal: 'SIGTERM'
+	    			}
+	    		},
+	    		command: 'node app.js'
+	    	}
+	    },
+	    concurrent: {
+	    	target: {
+	    		tasks: ['express:dev:stop' ,'express:dev'],
+	    		options: {
+	    			logConcurrentOutput: true
+	    		}
+	    	}
+	    }
+	});
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-handlebars');
+	grunt.loadNpmTasks('grunt-nodemon');
+	grunt.loadNpmTasks('grunt-concurrent');
+	grunt.loadNpmTasks('grunt-express-server');
+	grunt.loadNpmTasks('grunt-shell')
+
+	grunt.registerTask('default', ['express:dev', 'jshint', 'watch', 'handlebars']);
+}
