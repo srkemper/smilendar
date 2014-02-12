b7ff68c27c53acc73487991c5016ba04279ab24a
JimW <abidingsailor@gmail.com>
2014-02-11 23:17:00 -0800

    added gcal files

--- app.js
+++ app.js
@@ -42,10 +42,63 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+
+app.get('/', function(req, res) {
+  var locals = {
+        title: 'This is my sample app',
+        url: gapi.url
+      };
+  res.render('index.jade', locals);
+});
+
+app.get('/homepage', routes.index);
 app.get('/users', user.list);
 app.get('/calendar_event', calendar_event.view);
 app.get('/month', month.view);
+
+// handling return value
+app.get('/oauth2callback', function(req, res) {
+  var code = req.query.code;
+//  console.log(code);
+  gapi.client.getToken(code, function(err, tokens) {
+    gapi.client.credentials = tokens;
+    getData();
+//    console.log(tokens);
+  });
+  var locals = {
+        title: 'This is my sample app',
+        url: gapi.url       // add gapi.url to the locals object that is sent to index.jade.
+      };
+  res.render('index.jade', locals);
+});
+
+app.get('/cal', function(req, res){
+  var locals = {
+    title: "These are your calendars",
+    user: my_profile.name,
+    bday: my_profile.birthday,
+    events: my_calendars,
+    email: my_email
+  };
+  res.render('cal.jade', locals);
+});
+
+var getData = function() {
+  gapi.oauth.userinfo.get().withAuthClient(gapi.client).execute(function(err, results){
+      console.log(results);
+      my_email = results.email;
+      my_profile.name = results.name;
+      my_profile.birthday = results.birthday;
+  });
+  gapi.cal.calendarList.list().withAuthClient(gapi.client).execute(function(err, results){
+    console.log('calendar results');
+    console.log(results);
+    for (var i = results.items.length - 1; i >= 0; i--) {
+      my_calendars.push(results.items[i].summary);
+    };
+  });
+};
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
