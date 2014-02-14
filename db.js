9fe08a7d46b0051d9c30d8eefd690d985e653de0
srkemper <srkemper@stanford.edu>
2014-02-14 00:47:40 -0800

    connecting to our mongolab db instead of a local copy

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-var databaseURI = "localhost:27017/smilendardb";
+var databaseURI = "mongodb://smilendar147:smilendar147@ds027819.mongolab.com:27819/heroku_app21990328";
 var collections = ["events"];
 var db = require("mongojs").connect(databaseURI, collections);
 
