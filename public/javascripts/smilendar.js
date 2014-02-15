df1376d6bda85c961196c5033cdbbdc7ca85941a
ZayneS <zheng.shen05@gmail.com>
2014-02-15 15:54:26 -0800

    Fix the problem that when choosing the smile, user is redirected to event detail page. By dynamically add event handler to AJAX created content

--- public/javascripts/smilendar.js
+++ public/javascripts/smilendar.js
@@ -1,37 +1,63 @@
 
-
 $(document).ready(function(){
     initializePage();
 })
+
+
 function initializePage() {
     urlToPass ='/dayEvent/wed'
+    // Load the event list of today
     $.get(urlToPass, renderDayEvent);
-    $(".smile").on('click','.mood-status',chooseMood);
-    $('.cal-cell1').on('click',selectDay);
     console.log('initialize page');
 }
 
-function chooseMood(e) {
-  e.preventDefault();
-  console.log("did it!");
-  var mood = $(this).attr('id');
-  console.log(mood);
-  $(this).parent().siblings(".mood-display").attr('id',mood);
 
-  // database magic
-  var dbid = $(this).parent().siblings(".mood-display").attr('data-identifier');
-  console.log(dbid);
-  $.ajax({
-  	url: '/changeMood',
-  	type: 'POST',
-  	success: function(){console.log("ajax post success")},
-  	contentType: 'application/json',
-  	data: JSON.stringify({id:dbid, mood: mood})
-  });
+function renderDayEvent(result) {
+  // Using handlebars template to render event list of a day
+	console.log("renderDayEvent results:");
+    console.log(result);
+
+    // Retrieve templates from template file
+    var template = Smilendar.Templates["templates/eventList.handlebars"];
+    Handlebars.registerPartial('eventItem', Smilendar.Templates["templates/eventItem.handlebars"]);
+    var htmlText = template(result);
+    $('#this_day').html(htmlText);
+
+    // append event handler to AJAX created content
+    initDynamicEventHandlers();
+}
+
+function initDynamicEventHandlers() {
+    // Dynamically append event handler to AJAX created content.
+    console.log('initialized!');
+    $(".smile").on('click','.mood-status',chooseMood);
+    $('.cal-cell1').on('click',selectDay);
+
+}
+
+function chooseMood(e) {
+    // Choose the mood for according event
+    e.preventDefault();
+    console.log("did it!");
+    var mood = $(this).attr('id');
+    console.log(mood);
+    $(this).parent().siblings(".mood-display").attr('id',mood);
+
+    // Update mood to database using AJAX
+    var dbid = $(this).parent().siblings(".mood-display").attr('data-identifier');
+    console.log(dbid);
+    $.ajax({
+     url: '/changeMood',
+     type: 'POST',
+     success: function(){console.log("ajax post success")},
+     contentType: 'application/json',
+     data: JSON.stringify({id:dbid, mood: mood})
+    });
 }
 
 function selectDay(e) {
-    event.preventDefault();
+    // Retrieve event list of a day using AJAX
+    e.preventDefault();
     console.log('selecting day-----');
     var day = $(this).attr('id');
     console.log(day);
@@ -39,14 +65,4 @@ function selectDay(e) {
     $.get(urlToPass, renderDayEvent);
     console.log(urlToPass);
 
-}
-
-function renderDayEvent(result) {
-	console.log("renderDayEvent results:");
-    console.log(result);
-    console.log('this is template: ' + Smilendar.Templates["templates/eventList.handlebars"]);
-    var template = Smilendar.Templates["templates/eventList.handlebars"];
-    Handlebars.registerPartial('eventItem', Smilendar.Templates["templates/eventItem.handlebars"]);
-    var htmlText = template(result);
-    $('#this_day').html(htmlText);
 }
