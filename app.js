3687ea80c9e12f2a4444f4d205532f20e00bf413
srkemper <srkemper@stanford.edu>
2014-02-18 21:10:13 -0800

    added Event.moodByMonth method, which returns an array of moods for each given date of each month

--- app.js
+++ app.js
@@ -16,12 +16,11 @@ var handlebars = require('express3-handlebars');
 // var gapi = require('./routes/gapi');
 var mongo_client = require('mongodb').MongoClient;
 var mongojs = require('mongojs');
-var mongoose = require('mongoose');
 
 var db = require('./db');
 
+var mongoose = require('mongoose');
 var mongoosedb = require('./mongoose');
-
 var Event = mongoose.model('Event');
 var User = mongoose.model('User');
 
--- mongoose.js
+++ mongoose.js
@@ -33,7 +33,7 @@ eventSchema.virtual('start_year').get(function() {
 
 // static findByDate method that takes in a date and returns a list of all the events on that date
 // eventually will be extended to events for a specific user on a specific date
-eventSchema.statics.findByDate = function(date, cb) {
+eventSchema.statics.findByDate = function(date, callback) {
   var eventList = [];
   var date = new Date();
   this.find({}, function(err, events) {
@@ -42,12 +42,33 @@ eventSchema.statics.findByDate = function(date, cb) {
         eventList.push(eve);
       }
     });
-    cb(err, eventList);
+    callback(err, eventList);
   });
 };
+
+eventSchema.statics.moodByMonth = function(month, year, callback) {
+  var moodForDay = {}
+  this.find({}, function(err, events) {
+    events.forEach(function(eve) {
+      if (eve.start_month == month && eve.start_year == year) {
+        var today = moodForDay[eve.start_day];
+        if (today == null) {
+          moodForDay[eve.start_day] = [eve.mood];
+        } else {
+          moodForDay[eve.start_day].push(eve.mood);
+        }
+      }
+    });
+    callback(err, moodForDay);
+  });
+};
+
 mongoose.model('Event', eventSchema);
 var Event = mongoose.model('Event');
 
+// Event.moodByMonth(new Date().getMonth(), new Date().getYear(), function(err, moods) {
+//   console.log(moods);
+// });
 // example of how the static findByDate method works in Event
 // Event.findByDate(new Date(), function(err, events) {
 //   console.log("--------Event.findByDate--------------")
