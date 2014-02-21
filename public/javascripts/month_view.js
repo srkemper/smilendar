6015dd75eccd974b210769fcc8a5c31869b1475d
ZayneS <zheng.shen05@gmail.com>
2014-02-20 21:56:36 -0800

    Switch b/w smile and calendar in month view page

--- public/javascripts/month_view.js
+++ public/javascripts/month_view.js
@@ -1,6 +1,9 @@
 
 $(document).ready(function(){
-    initializeDayView();
+    console.log('this is month view!!');
+    initializeEventView();
+    $(".to-cal").on("click",hideMood);
+    $(".to-mood").on("click",showMood);
     // $(".weekday-num").on("click",".cal-cell1", selectDay);
 })
 
@@ -8,4 +11,19 @@ $(document).ready(function(){
 function initializeEventView() {
     console.log('initialize page');
     initDynamicEventHandlers();
+}
+
+
+function showMood() {
+  console.log('show mood!');
+  $(".month-view").addClass('show-mood');
+  $(".to-cal").removeClass("active");
+  $(".to-mood").addClass("active");
+}
+
+function hideMood() {
+  console.log('hide mood!');
+  $(".month-view").removeClass('show-mood');
+  $(".to-mood").removeClass("active");
+  $(".to-cal").addClass("active");
 }
