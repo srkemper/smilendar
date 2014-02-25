cb2350ecba61fc6cbdee5960b1d5c15838d504c6
srkemper <srkemper@stanford.edu>
2014-02-25 01:01:59 -0800

    fixing issue of username not appearing on add event page

--- routes/addEvent.js
+++ routes/addEvent.js
@@ -4,7 +4,7 @@
 exports.view = function(req, res) {
 	console.log(req.params.id)
     res.render('addEvent', {
-    	user:req.session.username,
+    	username:req.session.username,
     	script:"/javascripts/add_event.js",
     	dayId:req.params.id,
     	goback:{
